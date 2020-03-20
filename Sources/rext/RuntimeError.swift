38137f1e76cd6aef80c4a5482b0a0e42ad883278
Bryce Campbell <tonyhawk2100@gmail.com>
2020-03-20 11:06:40 -0700

    create error enumeration.

---
+++ Sources/rext/RuntimeError.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+enum RunTimeError: Error, CustomStringConvertible {
+    case missingExtensions, missingExtension, missingReplacement
+
+    var description: String {
+        switch self {
+            case .missingExtensions: return "Please specify both extensions."
+            case .missingExtension: return "Please specify extension to rename."
+            case .missingReplacement: return "Please specify replacement extension."
+        }
+    }
+}
