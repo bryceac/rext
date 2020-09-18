e680fc79323a212022ec2127923f24a6e99225fe
Bryce <tonyhawk2100@gmail.com>
2020-09-18 09:39:48 -0700

    fix package manifest

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .target(
             name: "rext",
             dependencies: [
-                .product("ArgumentParser", package: "swift-argument-parser")
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]),
         .testTarget(
             name: "rextTests",
