1cf84b99ac176d79a82c49be857245a594fe43be
Bryce <tonyhawk2100@gmail.com>
2020-09-18 09:41:37 -0700

    make more code changes

--- Sources/rext/Rext.swift
+++ Sources/rext/Rext.swift
@@ -7,7 +7,7 @@ struct Rext: ParsableCommand {
     static let configuration = CommandConfiguration(abstract: "quickly renames all files with a specified extension with another specified extension")
     
     // specify options, flags, and arguments, allowing use of short and long flags on each option and flag
-    @Option(name: .shortAndLong, default: ".", help: "Specifies the directory to go through.") var dir: String
+    @Option(name: .shortAndLong, help: "Specifies the directory to go through.") var dir: String = "."
     @Argument() var ext: String
     @Argument() var newExtension: String
     @Flag(name: .shortAndLong, help: "recursively change extensions.") var recursive: Bool = false
@@ -28,7 +28,7 @@ struct Rext: ParsableCommand {
         let FILE_MANAGER = FileManager.default
 
         // attempt to grab list of files and folders
-        guard let CONTENTS = !includeHidden ? try? FILE_MANAGER.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil) : try?  FILE_MANAGER.contentsOfDirectory(at: directory, includingPropertiesForKeys: ni, options: [.skipsHiddenFiles]) else {
+        guard let CONTENTS = !includeHidden ? try? FILE_MANAGER.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil) : try?  FILE_MANAGER.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles]) else {
             fatalError("cannot read directory!")
         }
 
